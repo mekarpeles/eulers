48402f7f871b29d02067b0dfdf888761eade89b2
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-03-16 00:22:30 -0700

    Finished e2

--- py/e2.py
+++ py/e2.py
@@ -1,13 +1,34 @@
+def memoize(f):
+    """ Memoization decorator for functions taking one or more arguments. """
+    class memod(dict):
+        def __getitem__(self, *key):
+            return dict.__getitem__(self, key)
+        def __missing__(self, key):
+            ret = self[key] = f(*key)
+            return ret
+    return memod().__getitem__
 
+@memoize
 def fib(n):
-    if n < 2:
-        return 1
-    return fib(n-1) + fib(n-2)
+    return n if n < 2 else fib(n-1) + fib(n-2)
+
+def even(x):
+    return not(x % 2)
     
 def euler_2(n):
     return fib(n)
 
 if __name__ == "__main__":
-    for i in range(10):
-        print euler_2(i)
-
+    goalp = False
+    i = 1
+    accum = 0
+    tmp = 0
+    while not(goalp):
+        prev = tmp
+        tmp = euler_2(i)
+        if tmp + prev > 4000000:
+            goalp = True
+        elif even(tmp):
+            accum += tmp
+        i += 1
+    print accum
