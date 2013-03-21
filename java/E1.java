1a425777b7ae6fc9364beaa55679f276984eba1c
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-03-21 04:39:55 -0700

    java sol to e1

---
+++ java/E1.java
@@ -0,0 +1,11 @@
+public class E1 {
+    public static void main (String[]args) {
+	int sum = 0;
+	for(int i=0; i<1001; ++i) {
+	    if(i%3==0 || i%5==0) {
+		sum += i;
+	    }
+	}
+	System.out.println(sum);
+    }
+}
